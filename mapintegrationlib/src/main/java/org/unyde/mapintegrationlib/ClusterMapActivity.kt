1303a865b67bb475ece2a7927efabe67ead68734
Vivek <gates.vivek@gmail.com>
2020-03-03 16:03:36 +0530

    cluster store point

--- mapintegrationlib/src/main/java/org/unyde/mapintegrationlib/ClusterMapActivity.kt
+++ mapintegrationlib/src/main/java/org/unyde/mapintegrationlib/ClusterMapActivity.kt
@@ -1,8 +1,9 @@
 package org.unyde.mapintegrationlib
 
+import android.Manifest
 import android.app.ProgressDialog
-import android.content.ComponentName
-import android.content.Intent
+import android.content.*
+import android.content.pm.PackageManager
 import android.graphics.drawable.InsetDrawable
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -12,6 +13,7 @@ import android.net.wifi.WifiManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.view.Window
@@ -41,7 +43,8 @@ import org.unyde.mapintegrationlib.util.Constants
 import org.unyde.mapintegrationlib.util.Pref_manager
 import org.unyde.mapintegrationlib.util.viewpagerAdsCard.AnchorBottomSheetBehavior
 import java.lang.Float
-import java.util.ArrayList
+import java.util.*
+import kotlin.collections.HashMap
 
 class ClusterMapActivity : AppCompatActivity(), FloorClickListner, SceneLoader.Callback,
     Cluster3DMap.CalorieStepsCallback {
@@ -236,6 +239,22 @@ class ClusterMapActivity : AppCompatActivity(), FloorClickListner, SceneLoader.C
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cluster_full_map)
+        wifiManager = getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
+        // progressBar = findViewById(R.id.progressBar) as ProgressBar
+        if (!wifiManager!!.isWifiEnabled())
+        {
+            Toast.makeText(this, "WiFi is disabled ... We need to enable it", Toast.LENGTH_LONG).show()
+            wifiManager!!.setWifiEnabled(true)
+        }
+
+        scan_mode = true
+        count = 0
+        myMultiMap.clear()
+        mydev_name.clear()
+        runnable.run()
+        gson = Gson()
+        root = Root()
+        root = gson!!.fromJson(json_val, Root::class.java)
         cluster_id = getIntent().getStringExtra("cluster_id")
         mall_name = getIntent().getStringExtra("mall_name")
         mall_address = getIntent().getStringExtra("mall_address")
@@ -306,7 +325,242 @@ class ClusterMapActivity : AppCompatActivity(), FloorClickListner, SceneLoader.C
         }
 
     }
+    private val handler = Handler()
+    private val runnable: Runnable = object : Runnable {
+        override fun run() {
+            scanWifi()
+            if (scan_mode) {
+                handler.postDelayed(this, 2000)
+            }
+        }
+    }
+    internal var wifiReceiver: BroadcastReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context:Context, intent:Intent) {
+            results = wifiManager!!.getScanResults()
+            unregisterReceiver(this)
+            for (scanResult in results!!)
+            {
+                if (!myMultiMap.containsKey(scanResult.BSSID))
+                {
+                    val abc = ArrayList<Int>()
+                    abc.add(scanResult.level)
+                    myMultiMap.put(scanResult.BSSID, abc)
+                    mydev_name.put(scanResult.BSSID, scanResult.SSID)
+                }
+                else
+                {
+                    val abc = myMultiMap.get(scanResult.BSSID) as ArrayList<Int>
+                    abc.add(scanResult.level)
+                    myMultiMap.put(scanResult.BSSID, abc!!)
+                }
+                arrayList.add(scanResult.SSID + " - " + scanResult.BSSID + " - " + scanResult.level)
+                //adapter.notifyDataSetChanged();
+            }
+            //value.setText(key_val);
+        }
+    }
 
+    private fun scanWifi() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) !== PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(getApplicationContext(), "Give Location permission to this application", Toast.LENGTH_SHORT).show()
+            requestPermissions(arrayOf<String>(Manifest.permission.ACCESS_COARSE_LOCATION), 101)
+        }
+        else
+        {
+            //scanning();
+            count++
+            //  progressBar!!.setProgress(count * 16)
+            //value.setText("Locating....");
+            arrayList.clear()
+            registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+            wifiManager!!.startScan()
+            //Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+            if (count > 5)
+            {
+                //value.setText("Locating.");
+                //  progressBar!!.setProgress(count * 16)
+                count = 0
+                scan_mode = false
+                //handler.removeCallbacks(runnable);
+                // wifiManager.();
+                // Toast.makeText(this, "Getnearest point", Toast.LENGTH_SHORT).show();
+                Log.d("Vivek tag", "Getnearest point")
+                get_nearest_point()
+            }
+        }
+    }
+
+    private fun get_nearest_point() {
+        val itr = myMultiMap.keys.iterator()
+        while (itr.hasNext())
+        {
+            val key = itr.next()
+            val value = myMultiMap.get(key)
+            //Log.d("Vivek Key",key+"Size:"+value.size());
+            //Toast.makeText(getApplicationContext(),key+myMultiMap.keySet().size(),Toast.LENGTH_SHORT).show();
+            var sum = 0
+            for (i in value!!.indices)
+            {
+                sum += value!!.get(i)
+            }
+            mydevsignal_mapper.put(key, sum / value!!.size)
+            // Toast.makeText(getApplicationContext(),"Size"+myMultiMap.keySet().size(),Toast.LENGTH_SHORT).show();
+        }
+        val s = root!!.getPoint_list()
+        var nearest_point_status = false
+        var landmark_index = -1
+        var point_distance = 10000
+        var sorted_store = HashMap<Int, Int>()
+        var passed_point = HashMap<String, HashMap<String, Int>>()
+        for (i in s.indices)
+        {
+            Log.d("Vivek Key", "Data Set Size:" + s.size)
+            val signal_list = root!!.getPoint_list().get(i).getSignal_val()
+            nearest_point_status = true
+            val hwid_list = HashMap<String, Int>()
+            val hwid_nm_list = HashMap<String, Int>()
+
+            for (j in signal_list.indices)
+            {
+                /// Log.d("Vivek Key","Signal List Size:"+signal_list.size()+""+ root.getPoint_list().get(i).getSignal_val().get(j).getHw_id());
+                if (myMultiMap.containsKey(signal_list.get(j).getHw_id()) )//&& ((mydevsignal_mapper.get(signal_list.get(j).getHw_id()))> -70))
+                {
+                    var diff = signal_list.get(j).getHw_value() - (mydevsignal_mapper.get(signal_list.get(j).getHw_id())!!)
+                    //Toast.makeText(getApplicationContext(),""+diff,Toast.LENGTH_SHORT).show();
+                    if (diff < 0)
+                    {
+                        diff = -1 * diff
+                    }
+                    if (diff <= 15)
+                    {
+                        point_distance += diff
+                        Log.d("Matched", root!!.getPoint_list().get(i).getName() + "!" + diff + "!!" + signal_list.get(j).getHw_id() + "||" + signal_list.get(j).getHw_value() + " || " + (mydevsignal_mapper.get(signal_list.get(j).getHw_id())))
+                        hwid_list.put(signal_list.get(j).getHw_id(), diff)
+                    }
+                    else
+                    {
+                        Log.d("NotMatched", root!!.getPoint_list().get(i).getName() + "!" + diff + "!!" + signal_list.get(j).getHw_id() + "||" + signal_list.get(j).getHw_value() + " || " + (mydevsignal_mapper.get(signal_list.get(j).getHw_id())))
+                        nearest_point_status = false
+                    }
+                }
+            }
+            if (nearest_point_status)
+            {
+                landmark_index = i
+                sorted_store.put(point_distance, i)
+                Log.d("All Matched", "Checked in" + root!!.getPoint_list().get(landmark_index).getName())
+                passed_point.put(root!!.getPoint_list().get(landmark_index).getName(), hwid_list)
+                //buttonScan.setText("Checked in"+root.getPoint_list().get(landmark_index).getName());
+            }
+        }
+        var checkedin_store = ""
+        var Al3 = ArrayList<String>()
+        var prime_set = HashSet<String>()
+        if (sorted_store.size > 0)
+        {
+            // buttonScan.setText(buttonScan.getText()+""+sorted_store.size());
+            val sorted = TreeMap<Int, Int>(sorted_store)
+            val mappings = sorted.entries
+            for (mapping in mappings)
+            {
+                checkedin_store += (root!!.getPoint_list().get(mapping.value).getName())
+                // Log.d("Matched Passed Poirnt",""+mapping.getValue()+""+(root.getPoint_list().get(mapping.getValue()).getName()));
+                if (prime_set.size == 0)
+                {
+                    val te=passed_point.get((root!!.getPoint_list().get(mapping.value).getName()))!!.keys.toMutableSet()
+                    prime_set.retainAll(te)
+                }
+                else
+                {
+                    val temp_set = passed_point.get((root!!.getPoint_list().get(mapping.value).getName()))!!.keys
+                    prime_set.retainAll(temp_set)
+                }
+            }
+        }
+        else
+        {
+            Log.d("Out Side loop", " Not Checked in any store")
+            // buttonScan.setText("Finding....");
+        }
+        Log.d("Matched", "-----------")
+        val itr2 = passed_point.keys.iterator()
+        val lm_point = HashMap<Int, String>()
+        while (itr2.hasNext())
+        {
+            var key = itr2.next()
+            var lmpoint_value = passed_point.get(key)
+            var temp_dist = 0
+            var itr3 = lmpoint_value!!.keys.iterator()
+            var key_temp = ""
+            while (itr3.hasNext())
+            {
+                key_temp = itr3.next()
+                if (prime_set.contains(key_temp))
+                {
+                    temp_dist += lmpoint_value!!.get(key_temp)!!
+                    Log.d("Matched", "" + key_temp + "!" + lmpoint_value.get(key_temp))
+                    //passed_point.containsValue()
+                    //temp_dist =
+                }
+            }
+            Log.d("Matched", "" + temp_dist)
+            Log.d("Matched", "" + "------------------------------------" + key + "----")
+            lm_point.put(temp_dist, key)
+            //buttonScan.setText(buttonScan.getText()+""+key+"!"+temp_dist);
+        }
+        var sorted1 = TreeMap<Int, String>(lm_point)
+        var mappings = sorted1.entries
+        var store_marker = ArrayList<Marker_Internal_Nav>()
+        store_marker!!.clear()
+        store_marker!!.add(Marker_Internal_Nav(0f,0f,0f,"A","N","B","D"));
+        cluster3DMap!!.setStoreMarkers(floor, store_marker)
+        for (mapping1 in mappings)
+        {
+            //root.getPoint_list().get(mapping1.getValue()).getName();
+            // buttonScan.setText(mapping1.getValue());
+            if (mapping1.value.toString() == "chaayos")
+            {
+              /*  val openpop = Intent(this@MainActivity, PopUpActivityWifiSignal::class.java)
+                openpop.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                openpop.putExtra("brands_name", "Chaayos")
+                openpop.putExtra("numstores", "GF,Logix Center")
+                openpop.putExtra("offer_count_store", "5")
+                openpop.putExtra("coupon_count_txt", "7")*/
+                //startActivity(openpop)
+
+            }
+            else if (mapping1.value.toString() == "haldiram")
+            {
+                /*val openpop = Intent(this@MainActivity, PopUpActivityWifiSignal::class.java)
+                openpop.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                openpop.putExtra("brands_name", "Haldiram")
+                openpop.putExtra("numstores", "GF,Logix Center")
+                openpop.putExtra("offer_count_store", "5")
+                openpop.putExtra("coupon_count_txt", "7")*/
+                //startActivity(openpop)
+            }
+            else if (mapping1.value.toString() == "blackberry")
+            {
+               /* val openpop = Intent(this@MainActivity, PopUpActivityWifiSignal::class.java)
+                openpop.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                openpop.putExtra("brands_name", "Blackberry")
+                openpop.putExtra("numstores", "GF,Logix Center")
+                openpop.putExtra("offer_count_store", "5")
+                openpop.putExtra("coupon_count_txt", "7")*/
+                //startActivity(openpop)
+            }
+            break
+        }
+        //buttonScan.setText(checkedin_store);
+        //.setText(checkedin_store);
+        mydev_name.clear()
+        myMultiMap.clear()
+        mydevsignal_mapper.clear()
+        count = 0
+        //progressBar!!.setProgress(count * 16)
+        scan_mode = true
+    }
 
     override fun onFloorItemClick(pos: Int) {
         try {
@@ -383,7 +637,7 @@ class ClusterMapActivity : AppCompatActivity(), FloorClickListner, SceneLoader.C
                         .db.pathNodeList().getCordinatesForMarkers(mall_brand!!, "0");
                 var store_marker = ArrayList<Marker_Internal_Nav>()
                 store_marker!!.clear()
-                if (markers.size > 0) {
+               /* if (markers.size > 0) {
                     for (i in 0 until markers!!.size) {
                         store_marker!!.add(
                             Marker_Internal_Nav(
@@ -413,7 +667,8 @@ class ClusterMapActivity : AppCompatActivity(), FloorClickListner, SceneLoader.C
 
                     }
                     cluster3DMap!!.setStoreMarkers(0, store_marker)
-                }
+                }*/
+                store_marker!!.add(Marker_Internal_Nav(0f,0f,0f,"A","N","B","D"));
             } else {
                 var cn = ComponentName(this@ClusterMapActivity, "woogly.unyde.org.wooglyunyde.activities.BrandPageAct");
                 var intent = Intent().setComponent(cn);
